278e98449e07ec74ef93d3ff36671b8750863392
songlinhao <455674848@qq.com>
2018-09-07 19:55:49 +0800

    添加gateway

---
+++ gateway/src/main/java/org/demon/GateWayApplication.java
@@ -0,0 +1,17 @@
+package org.demon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author demon
+ * @version 1.0
+ * @date 2018/9/6 11:11
+ * @since 1.0
+ */
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
