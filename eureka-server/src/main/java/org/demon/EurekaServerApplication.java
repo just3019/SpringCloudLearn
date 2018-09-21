3dc33a764942d690439e2ec31949a3b0c7cfef1d
songlinhao <455674848@qq.com>
2018-09-21 14:18:09 +0800

    eureka整合hystrix 监控界面。

--- eureka-server/src/main/java/org/demon/EurekaServerApplication.java
+++ eureka-server/src/main/java/org/demon/EurekaServerApplication.java
@@ -2,10 +2,13 @@ package org.demon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableHystrixDashboard
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
--- eureka-test/src/main/java/org/demon/EurekaTestApplication.java
+++ eureka-test/src/main/java/org/demon/EurekaTestApplication.java
@@ -1,17 +1,24 @@
 package org.demon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @RestController
+@EnableCircuitBreaker
 public class EurekaTestApplication {
 
     public static void main(String[] args) {
@@ -24,6 +31,16 @@ public class EurekaTestApplication {
     @RequestMapping("/hi")
     public String home(@RequestParam(value = "name", defaultValue = "demon") String name) {
         return "hi " + name + " ,i am from port:" + port;
+    }
+
+    @RequestMapping("/test")
+    @HystrixCommand(fallbackMethod = "hasError")
+    public String test() throws Exception {
+        throw new Exception("hall");
+    }
+
+    public String hasError() {
+        return "error!!!";
     }
 
     @Autowired
